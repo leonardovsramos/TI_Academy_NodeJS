aa49efced8a755392665b878f1c9b615f62f3d8d
LeonardoVSRamos <leonardovsr.dev@gmail.com>
2022-02-09 10:33:57 -0300

    Curso TI Academy - Módulo 03 - NodeJS - ERRO: inclusão de Foreign Key não realizada na rota /pedidos

--- Controller.js
+++ Controller.js
@@ -1,23 +1,52 @@
 const express = require("express");
 const cors = require("cors");
 
+const models = require("./models");
+
 const app = express();
 app.use(cors());
+
+let cliente = models.Cliente;
+let itempedido = models.ItemPedido;
+let pedido = models.Pedido;
+let servico = models.Servico;
 
 app.get("/", function(req, res) {
     res.send("Olá, Mundo!");
 });
 
-app.get("/clientes", function(req, res) {
-    res.send("Sejam bem-vindo(a) a ServicesTI!");
+app.get("/servicos", async(req, res) => {
+    await servico.create({
+        nome: "HTML e CSS",
+        descricao: "Curso desenvolvido pela TI Academy, focado no desenvolvimento de páginas estáticas com HTML5 e CSS3",
+        createAt: new Date(),
+        updateAt: new Date()
+    });
+    res.send("O serviço foi criado com sucesso");
+})
+
+app.get("/clientes", async(req, res) => {
+    await cliente.create({
+        nome: "Eduardo Vieira",
+        endereco: "Rua rosa cruz, 411",
+        cidade: "Maringá",
+        uf: "PR",
+        nascimento: new Date("1950-02-14"),
+        clienteDesde: new Date("2018-06-17"),
+        createAt: new Date(),
+        updateAt: new Date()
+    });
+    res.send("Cliente cadastrado com Sucesso!");
 });
 
-app.get("/servicos", function(req, res) {
-    res.send("Aba Serviços - Qual serviço gostaria de fazer?");
-});
-
-app.get("/pedidos", function(req, res) {
-    res.send("Qual pedido gostaria de fazer?");
+app.get("/pedidos", async(req, res) => {
+    await pedido.create({
+        data: new Date("2022-02-09"),
+        // ClienteId: ,
+        createAt: new Date(),
+        updateAt: new Date()
+    });
+    res.send("Pedido realizado com Sucesso!")
 });
 
 let port = process.env.port || 3001;
